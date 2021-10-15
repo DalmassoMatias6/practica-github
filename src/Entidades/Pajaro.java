3b35265d325fabeb13c3d6caf28855a9aa5cf8e2
José Pastrana Arg <jose.pastrana.ar@gmail.com>
2021-10-15 18:57:17 -0300

    RamaJP Primer Commit - Creación de clase Pajaro

--- src/Entidades/Pajaro.java
+++ src/Entidades/Pajaro.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -64,3 +65,4 @@ public class Pajaro {
     }
 
 }
+
