51fbbd9fb8bfd058b5b264b39e6a2c0a5d1d84b5
Matias Dalmasso <dalmassomatias6@gmail.com>
2021-10-15 19:36:27 -0300

    Cambios en la clase Pato

--- src/Entidades/Pato.java
+++ src/Entidades/Pato.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Entidades;
 
-/**
- *
- * @author dalma
- */
 public class Pato {
+    private String nombre;
+    private int edad;
+    private int cantPlumas;
+    
+//-----------------------------------------------------------------------------------
+    //constructores
+
+    public Pato() {
+    }
+
+    public Pato(String nombre, int edad, int cantPlumas) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.cantPlumas = cantPlumas;
+    }
+    
+//-------------------------------------------------------------------------------------
+    //metodos get y set
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCantPlumas() {
+        return cantPlumas;
+    }
+
+    public void setCantPlumas(int cantPlumas) {
+        this.cantPlumas = cantPlumas;
+    }
+    
+//--------------------------------------------------------------------------------
+    //To string
+
+    @Override
+    public String toString() {
+        return "Pato{" + "nombre=" + nombre + ", edad=" + edad + ", cantPlumas=" + cantPlumas + '}';
+    }
     
 }
